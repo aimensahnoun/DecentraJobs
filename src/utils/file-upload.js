3e32162aa93f9455c16102e97ecc13d316cb29c6
Aimen Sahnoun <aimen.sahnoun@lyntlabs.com>
2022-03-05 13:17:56 +0300

    ⚙️ Functionlity : Added file upload to IPFS

---
+++ src/utils/file-upload.js
@@ -0,0 +1,33 @@
+//Web3Storage import
+import { Web3Storage } from "web3.storage";
+import imageCompression from "browser-image-compression";
+
+
+var options = {
+  maxSizeMB: 1,
+  useWebWorker: true,
+};
+
+const token = process.env.NEXT_PUBLIC_STORAGE_API;
+const client = new Web3Storage({ token });
+
+export const uploadFile = async (file, type) => {
+  if (!file) return;
+
+  const fileName = file.name.replace(/ /g, "");
+
+  var blob = file.slice(0, file.size, file.type);
+  const newFile = new File([blob], fileName, {type: file.type});
+
+  let finalFile = file;
+
+  try {
+    if (type === "image") finalFile = await imageCompression(newFile, options);
+
+    const cid = await client.put([finalFile]);
+
+    return `${cid}.ipfs.dweb.link/${fileName}`;
+  } catch (e) {
+    console.log(e);
+  }
+};
